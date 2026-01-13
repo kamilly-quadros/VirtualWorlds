21b10205e041ef9ab1150af746e0ccb29631fafe
kamilly-quadros <kamiquadros20@gmail.com>
2026-01-12 21:56:51 -0400

    feat: testes

---
+++ VirtualWorlds.Test/BooksControllerTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualWorlds.Server.Controllers;
+using VirtualWorlds.Server.Data;
+using VirtualWorlds.Server.Models;
+using VirtualWorlds.Server.DTOs;
+
+namespace VirtualWorlds.Server.Tests.Controllers
+{
+    [TestClass]
+    public class BooksControllerTests
+    {
+        private VirtualWorldsDbContext? _context;
+        private BooksController? _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<VirtualWorldsDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new VirtualWorldsDbContext(options);
+
+            SeedDatabase();
+
+            _controller = new BooksController(_context);
+        }
+
+        private void SeedDatabase()
+        {
+            _context!.Books.AddRange(
+                new Book
+                {
+                    Id = 1,
+                    Name = "Livro A",
+                    Price = 50,
+                    Specifications = new Specification
+                    {
+                        Author = "Autor 1",
+                        OriginallyPublished = "2020",
+                        PageCount = 200,
+                        IllustratorJson = "[\"Ilustrador 1\"]",
+                        GenresJson = "[\"Fantasia\"]"
+                    }
+                },
+                new Book
+                {
+                    Id = 2,
+                    Name = "Livro B",
+                    Price = 30,
+                    Specifications = new Specification
+                    {
+                        Author = "Autor 2",
+                        OriginallyPublished = "2018",
+                        PageCount = 150,
+                        IllustratorJson = "[\"Ilustrador 2\"]",
+                        GenresJson = "[\"Aventura\"]"
+                    }
+                }
+            );
+
+            _context.SaveChanges();
+        }
+
+        [TestMethod]
+        public async Task GetBooks_DeveRetornarOkComLista()
+        {
+            var result = await _controller.GetBooks(
+                name: null,
+                author: null,
+                illustrator: null,
+                genre: null,
+                yearFrom: null,
+                yearTo: null,
+                orderByPrice: "asc");
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+
+            var books = okResult.Value as IEnumerable<BookJsonDto>;
+            Assert.IsNotNull(books);
+            Assert.AreEqual(2, books.Count());
+        }
+
+        [TestMethod]
+        public async Task GetBooks_OrderByPriceAsc_DeveOrdenarCorretamente()
+        {
+            var result = await _controller.GetBooks(
+                null, null, null, null, null, null, "asc");
+
+            var okResult = result as OkObjectResult;
+            var books = okResult.Value as IEnumerable<BookJsonDto>;
+
+            Assert.AreEqual(30, books.First().Price);
+        }
+
+        [TestMethod]
+        public async Task GetBooks_OrderByPriceDesc_DeveOrdenarCorretamente()
+        {
+            var result = await _controller.GetBooks(
+                null, null, null, null, null, null, "desc");
+
+            var okResult = result as OkObjectResult;
+            var books = okResult.Value as IEnumerable<BookJsonDto>;
+
+            Assert.AreEqual(50, books.First().Price);
+        }
+
+        [TestMethod]
+        public async Task GetBooks_QuandoErro_DeveRetornar500()
+        {
+            var options = new DbContextOptionsBuilder<VirtualWorldsDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new VirtualWorldsDbContext(options);
+            context.Dispose();
+
+            var controller = new BooksController(context);
+
+            var result = await controller.GetBooks(
+                null, null, null, null, null, null, null);
+
+            Assert.IsInstanceOfType(result, typeof(ObjectResult));
+
+            var objectResult = result as ObjectResult;
+            Assert.AreEqual(500, objectResult.StatusCode);
+        }
+
+    }
+}
---
+++ VirtualWorlds.Test/ShippingControllerTest.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualWorlds.Server.Controllers;
+using VirtualWorlds.Server.Data;
+using VirtualWorlds.Server.Models;
+
+namespace VirtualWorlds.Server.Tests.Controllers
+{
+    [TestClass]
+    public class ShippingControllerTests
+    {
+        private VirtualWorldsDbContext? _context;
+        private ShippingController? _controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<VirtualWorldsDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new VirtualWorldsDbContext(options);
+
+            SeedDatabase();
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "Shipping:Percentage", "0.1" } 
+                })
+                .Build();
+
+            _controller = new ShippingController(_context, configuration);
+        }
+
+        private void SeedDatabase()
+        {
+            _context!.Books.AddRange(
+                new Book
+                {
+                    Id = 1,
+                    Name = "Livro A",
+                    Price = 100
+                },
+                new Book
+                {
+                    Id = 2,
+                    Name = "Livro B",
+                    Price = 50
+                }
+            );
+
+            _context.SaveChanges();
+        }
+
+        [TestMethod]
+        public async Task CalculateShipping_DeveRetornarOk_ComCalculoCorreto()
+        {
+            var bookIds = new List<int> { 1, 2 };
+
+            var result = await _controller.CalculateShipping(bookIds);
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+
+            var value = okResult.Value;
+
+            var totalBooksValue = (decimal)value.GetType()
+                .GetProperty("totalBooksValue")!
+                .GetValue(value)!;
+
+            var shippingValue = (decimal)value.GetType()
+                .GetProperty("shippingValue")!
+                .GetValue(value)!;
+
+            var totalWithShipping = (decimal)value.GetType()
+                .GetProperty("totalWithShipping")!
+                .GetValue(value)!;
+
+            Assert.AreEqual(150m, totalBooksValue);
+            Assert.AreEqual(15m, shippingValue);
+            Assert.AreEqual(165m, totalWithShipping);
+        }
+
+
+        [TestMethod]
+        public async Task CalculateShipping_IdsInvalidos_DeveRetornarBadRequest()
+        {
+            var bookIds = new List<int>();
+
+            var result = await _controller.CalculateShipping(bookIds);
+
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+        }
+
+        [TestMethod]
+        public async Task CalculateShipping_LivrosNaoEncontrados_DeveRetornarNotFound()
+        {
+            var bookIds = new List<int> { 999 };
+
+            var result = await _controller.CalculateShipping(bookIds);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+        }
+
+        [TestMethod]
+        public async Task CalculateShipping_QuandoErro_DeveRetornar500()
+        {
+            var options = new DbContextOptionsBuilder<VirtualWorldsDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new VirtualWorldsDbContext(options);
+            context.Dispose();
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "Shipping:Percentage", "0.1" }
+                })
+                .Build();
+
+            var controller = new ShippingController(context, configuration);
+
+            var result = await controller.CalculateShipping(new List<int> { 1 });
+
+            Assert.IsInstanceOfType(result, typeof(ObjectResult));
+
+            var objectResult = result as ObjectResult;
+            Assert.AreEqual(500, objectResult.StatusCode);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _context?.Dispose();
+        }
+    }
+}
